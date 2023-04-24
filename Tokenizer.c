c9a34778c28703ce8cdaf5a057ce50f13592982e
Alvarez0Jose <josealejandro220@gmail.com>
2023-04-24 16:24:08 +0000

    Adding the 5 necessary files for the basic shell to run

---
+++ Tokenizer.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+
+/**
+ * split_line - split command line into array of arguments
+ * @line: pointer to the line being split
+ * Return: the arguments split
+*/
+
+char **split_line(char *line)
+{
+	char **tokens = malloc(MAX_NUM_ARGS * sizeof(char *));
+	char *token;
+	int position = 0;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if (position >= MAX_NUM_ARGS)
+		{
+			fprintf(stderr, "Too many arguments\n");
+			exit(EXIT_FAILURE);
+		}
+
+		token = strtok(NULL, " \t\r\n\a");
+	}
+
+	tokens[position] = NULL;
+	return (tokens);
+}
---
+++ execute_command.c
@@ -0,0 +1,56 @@
+#include "shell.h"
+
+/**
+ *execute_command - helps execute a command in the prompt
+ *@command: the command being executed once it is written in the prompt line
+ *Return: Exit error if the command couldn't be executed
+*/
+
+void execute_command(char *line)
+{
+	pid_t PID;
+	int status;
+	char **args = split_line(line);
+	char full_path[MAX_COMMAND_LEN];
+
+	if (is_builtin(args[0]))
+	{
+		execute_builtin(args[0], args);
+		free(args);
+		return;
+	}
+
+	if (!command_exists(args[0], full_path))
+	{
+		fprintf(stderr, "%s: command not found\n", args[0]);
+		free(args);
+		return;
+	}
+
+	PID = fork();
+	if (PID == 0)
+	{
+		/* Child procces begins */
+		if (execve(full_path, args, environ) == -1)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+
+	}
+	else if (PID < 0)
+	{
+		/* fork error */
+		perror("Error");
+	}
+	else
+	{
+		/* Parent process */
+		do {
+			waitpid(PID, &status, WUNTRACED);
+		}
+
+		while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	free(args);
+}
