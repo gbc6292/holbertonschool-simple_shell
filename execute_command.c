8ed5d0b46b881161d24f67d8e93cb23e4279c852
Giovanni Barreto <124797071+gbc6292@users.noreply.github.com>
2023-10-18 22:02:18 -0400

    Documenting and cleaning the execute_command function

--- execute_command.c
+++ execute_command.c
@@ -1,63 +1,66 @@
 #include "shell.h"
+
 /**
- *execute_command - helps execute a command in the prompt
- *@line: the command being executed once it is written in the prompt line
- *Return: Exit error if the command couldn't be executed
-*/
+ * execute_command - Execute a command in the shell prompt.
+ * @line: The command to be executed, entered in the prompt line.
+ * Return: No explicit return value. Handles command execution and errors.
+ */
 void execute_command(char *line)
 {
-	pid_t PID;
-	int status;
-	char **args = split_line(line);
-	char *full_path = NULL;
+    pid_t PID;
+    int status;
+    char **args = split_line(line); // Split the input line into arguments
+    char *full_path = NULL;
 
-	if (is_builtin(args[0]))
-	{
-		execute_builtin(args[0], args);
-		return;
-	}
-	if (args[0][0] == '.' || args[0][0] == '/')
-	{
-		if (access(args[0], X_OK) == 0)
-		{
-			full_path = args[0];
-		}
-	}
-	else
-		full_path = command_exists(args[0]);
+    if (is_builtin(args[0]))
+    {
+        execute_builtin(args[0], args); // Execute a built-in command
+        return;
+    }
 
-	if (!full_path)
-	{
-		fprintf(stderr, "%s: command not found\n", args[0]);
-		free(args);
-		return;
-	}
-	PID = fork();
-	if (PID == 0)
-	{
-		/* Child procces begins */
-		if (execve(full_path, args, environ) == -1)
-		{
-			perror("Error");
-			exit(EXIT_FAILURE);
-		}
+    if (args[0][0] == '.' || args[0][0] == '/')
+    {
+        if (access(args[0], X_OK) == 0)
+        {
+            full_path = args[0]; // Use the provided path if it's executable
+        }
+    }
+    else
+        full_path = command_exists(args[0]); // Check if the command exists in the PATH
 
-	}
-	else if (PID < 0)
-	{
-		/* fork error */
-		perror("Error");
-	}
-	else
-	{
-		/* Parent process */
-		do {
-			waitpid(PID, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-	if (full_path != args[0])
-	free(full_path);
+    if (!full_path)
+    {
+        fprintf(stderr, "%s: command not found\n", args[0]); // Print an error if the command is not found
+        free(args); // Free memory allocated for arguments
+        return;
+    }
 
-	if (args)
-	free(args);
+    PID = fork(); // Create a new process
+    if (PID == 0)
+    {
+        /* Child process begins */
+        if (execve(full_path, args, environ) == -1)
+        {
+            perror("Error");
+            exit(EXIT_FAILURE); // Handle execution error
+        }
+    }
+    else if (PID < 0)
+    {
+        /* Fork error */
+        perror("Error");
+    }
+    else
+    {
+        /* Parent process */
+        do {
+            waitpid(PID, &status, WUNTRACED); // Wait for the child process to finish
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status);
+    }
+
+    if (full_path != args[0])
+        free(full_path); // Free memory allocated for the full_path
+
+    if (args)
+        free(args); // Free memory allocated for arguments
 }
